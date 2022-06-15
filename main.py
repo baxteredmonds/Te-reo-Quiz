4cc9946c3da44d16d0fc4852b3071fc31213ea40
Baxter Edmonds <101303762+baxteredmonds@users.noreply.github.com>
2022-06-15 02:59:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+answer1= input("what is Horse  te reo? \na. sand \nb. hoiho \nc. ground").lower()
+
+if answer1 == "a":
+  print ("Correct!")
+
+else: 
+  print ("Incorrect!")
+
+
+
+answer2= input("what is sgswg  te reo? \na. sand \nb. grass \nc. ground").lower()
+
+if answer2 == "c":
+  print ("Correct!")
+
+else: 
+  print ("Incorrect!")
