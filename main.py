d80c9416e0964fb6c7f668e4d6644d65977c2ffd
Baxter Edmonds <101303762+baxteredmonds@users.noreply.github.com>
2022-06-16 21:51:53 +0000

    first quiz code

--- main.py
+++ main.py
@@ -6,36 +6,3 @@ if answer1 == "a" or answer1 == "hoiho":
 else: 
   print ("Incorrect!")
 
-
-
-answer2= input("what is people in te reo? \na. Tīpuna \nb. Hikoi \nc. Tāngata \n").lower()
-
-if answer2 == "c" or answer2 == "tangata":
-  print ("Correct!")
-
-else: 
-  print ("Incorrect!")
-
-answer3= input("what is friend in te reo? \na. whaea \nb. hoa \nc. aroha \n").lower()
-
-if answer3 == "b" or answer3 == "hoa":
-  print ("Correct!")
-
-else: 
-  print ("Incorrect!")
-
-answer4= input("what is amazing in te reo? \na. tane \nb. Mīharo \nc. Moko \n").lower()
-
-if answer4 == "b" or answer4 =="Miharo":
-  print ("Correct!")
-
-else: 
-  print ("Incorrect!")
-
-answer5= input("what is land te reo? \na. Whenua \nb. Pukukai \nc. Titiro \n").lower()
-
-if answer5 == "a" or answer5 =="Whenua":
-  print ("Correct!")
-
-else: 
-  print ("Incorrect!")
