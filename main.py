40ab5f901f72daa1d76fa78ecc4cfaf8bd77343d
Baxter Edmonds <101303762+baxteredmonds@users.noreply.github.com>
2022-06-16 21:41:34 +0000

    i changed the or statment to say or ==answer to make it a multi answer statment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-answer1= input("what is Horse  te reo? \na. sand \nb. hoiho \nc. ground").lower()
+answer1= input("what is Horse in te reo? \na. Mōrena \nb. hoiho \nc. Tīpuna \n").lower()
 
-if answer1 == "a":
+if answer1 == "a" or answer1 == "hoiho":
   print ("Correct!")
 
 else: 
@@ -8,9 +8,33 @@ else:
 
 
 
-answer2= input("what is sgswg  te reo? \na. sand \nb. grass \nc. ground").lower()
+answer2= input("what is people in te reo? \na. Tīpuna \nb. Hikoi \nc. Tāngata \n").lower()
 
-if answer2 == "c":
+if answer2 == "c" or answer2 == "tangata":
+  print ("Correct!")
+
+else: 
+  print ("Incorrect!")
+
+answer3= input("what is friend in te reo? \na. whaea \nb. hoa \nc. aroha \n").lower()
+
+if answer3 == "b" or answer3 == "hoa":
+  print ("Correct!")
+
+else: 
+  print ("Incorrect!")
+
+answer4= input("what is amazing in te reo? \na. tane \nb. Mīharo \nc. Moko \n").lower()
+
+if answer4 == "b" or answer4 =="Miharo":
+  print ("Correct!")
+
+else: 
+  print ("Incorrect!")
+
+answer5= input("what is land te reo? \na. Whenua \nb. Pukukai \nc. Titiro \n").lower()
+
+if answer5 == "a" or answer5 =="Whenua":
   print ("Correct!")
 
 else: 
